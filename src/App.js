308397df3869c7aea17b36d74ce0c2a04e505bc1
Jose-Tejero <j_jtejero@hotmail.com>
2022-01-06 11:45:37 -0600

    Pensando en cambiar botones

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="buttons">
+        <button><ion-icon name="home-sharp"></ion-icon></button>
+        <button><ion-icon name="person-sharp"></ion-icon></button>
+        <button><ion-icon name="document-attach-sharp"></ion-icon></button>
+        <button><ion-icon name="chatbox-sharp"></ion-icon></button>
+      </div>
       <div className='Card'>
         Cara 1
-      </div>
-      <div className="buttons">
-        <button>Home</button>
-        <button>Profile</button>
-        <button>Proyects</button>
-        <button>Contact</button>
       </div>
     </div>
   );
